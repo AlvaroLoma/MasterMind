796d412e2e17b3184614671f5d13e95c459525ae
Alvaro lodeiro <mini_yitan@hotmail.com>
2018-05-17 14:09:22 +0200

    Archivo de texto 2.0

--- src/masterMind/Main.java
+++ src/masterMind/Main.java
@@ -1,10 +1,28 @@
 package masterMind;
 
+import java.io.IOException;
+
 public class Main {  
 
 	public static void main(String[] args) {
-
-		new Controlador();
+		Controlador controlador;
+		int salir=0;
+		do {
+		controlador=new Controlador();
+		salir++;
+//		try {
+//			controlador.fichero.write("<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		}while(salir<5);
+		try {
+			controlador.fichero.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
